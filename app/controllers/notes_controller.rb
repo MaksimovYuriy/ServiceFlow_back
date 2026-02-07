4965c684ec29b295b70403cc789cbd1de003ae72
Maksimov Yuriy <yura.maksimov.04@mail.ru>
2026-02-07 12:48:02 +0300

    Подтягивание клиента в систему

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,12 +2,35 @@ class NotesController < ApplicationController
   skip_before_action :authenticate!, only: [:create]
 
   def create
-    note = NoteResource.build(params)
-
+    debugger
+    client = Client.find_or_create_by!(phone: client_params[:phone])
+    
+    note = NoteResource.build(note_params(client))
     if note.save
       render jsonapi: note, status: 201
     else
       render jsonapi_errors: note
     end
+  end
+
+  private
+
+  def client_params
+    params.require(:data).require(:attributes).require(:client)
+  end
+
+  def note_params(client)
+    attrs = params.require(:data)
+                  .require(:attributes)
+                  .permit(:master_id, :service_id, :start_at, :end_at)
+
+    filtered_attrs = attrs.except(:client).merge(client_id: client.id)
+
+    filtered_params = {
+      data: {
+        type: params[:data][:type],
+        attributes: filtered_attrs
+      }
+    }
   end
 end
